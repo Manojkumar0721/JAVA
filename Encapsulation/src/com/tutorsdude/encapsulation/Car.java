d02dd5c3b4d1f8a21dd675294bb6e4c206990f85
Manojkumar0721 <manu07y@gmail.com>
2024-06-12 11:33:34 +0530

    Getter and Setter Examples

---
+++ Encapsulation/src/com/tutorsdude/encapsulation/Car.java
@@ -0,0 +1,234 @@
+package com.tutorsdude.encapsulation;
+
+public class Car {
+	private String brand = "VOLKSWAGEN";
+	private String carName = "POLO GT";
+	private String bodyType = "HatchBack";
+	private String driveLine = "Front-Wheel Drive";
+	private String transmission = "Manual";
+	private String fuel = "Petrol";
+	private String variants = "1.5 TDI Trendline";
+	private String features = "Cruise Control";
+	private String safety = "Parking Sensor";
+	private String toucthscreen = "Display";
+	private String wipers = "Rain Sensing Wipers";
+	private String steering = "Leather Finish";
+	private String interior = "Leather Finish";
+	private String sunroof = "With Sunroof";
+	private String headLamp = "Custom";
+	private String tailLamp = "Custom";
+	private int price = 500000;
+	private int cargoVolume = 280;
+	private int engine = 999;
+	private int power = 108;
+	private int torque = 95;
+	private int mileage = 21;
+	private int noOfCylinder = 3;
+	private int seatingCapacity = 5;
+	private int fuelTankCapacity = 45;
+	private int groundClearence = 165;
+	private int bootSpace = 280;
+	private int fuelEconomy = 17;
+	private int numOfDoars = 5;
+	private boolean isOutOfGas = false;
+	private boolean isHaveSpoiler = true;
+	
+	
+	public String getbrand() {
+		return brand;
+	}
+	public void setbrand(String brand) {
+		this.brand = brand;
+	}
+	public String getcarName() {
+		return carName;
+	}
+	public void setcarName(String carName) {
+		this.carName = carName;
+	}
+	public String getbodyType() {
+		return bodyType;
+	}
+	public void setbodyType(String bodyType) {
+		this.bodyType = bodyType;
+	}
+	public String getdriveLine() {
+		return driveLine;
+	}
+	public void setdriveLine(String driveLine) {
+		this.driveLine = driveLine;
+	}
+	public String gettransmission() {
+		return transmission;
+	}
+	public void settransmission(String transmission) {
+		this.transmission = transmission;
+	}
+	public String getfuel() {
+		return fuel;
+	}
+	public void setfuel(String fuel) {
+		this.fuel = fuel;
+	}
+	public String getvariants() {
+		return variants;
+	}
+	public void setvariants(String variants) {
+		this.variants = variants;
+	}
+	public String getfeatures() {
+		return features;
+	}
+	public void setfeatures(String features) {
+		this.features = features;
+	}
+	public String getsafety() {
+		return safety;
+	}
+	public void setsafety(String safety) {
+		this.safety = safety;
+	}
+	public String gettoucthscreen() {
+		return toucthscreen;
+	}
+	public void settoucthscreen(String toucthscreen) {
+		this.toucthscreen = toucthscreen;
+	}
+	public String getwipers() {
+		return wipers;
+	}
+	public void setwipers(String wipers) {
+		this.wipers = wipers;
+	}
+	public String getsteering() {
+		return steering;
+	}
+	public void setsteering(String steering) {
+		this.steering = steering;
+	}
+	public String getinterior() {
+		return interior;
+	}
+	public void setinterior(String interior) {
+		this.interior = interior;
+	}
+	public String getsunroof() {
+		return sunroof;
+	}
+	public void setsunroof(String sunroof) {
+		this.sunroof = sunroof;
+	}
+	public String getheadLamp() {
+		return headLamp;
+	}
+	public void setheadLamp(String headLamp) {
+		this.headLamp = headLamp;
+	}
+	public String gettailLamp() {
+		return tailLamp;
+	}
+	public void settailLamp(String tailLamp) {
+		this.tailLamp = tailLamp;
+	}
+	public int getprice() {
+		return price;
+	}
+	public void setprice(int price) {
+		this.price = price;
+	}
+	public int getcargoVolume() {
+		return cargoVolume;
+	}
+	public void setcargoVolume(int cargoVolume) {
+		this.cargoVolume = cargoVolume;
+	}
+	public int getengine() {
+		return engine;
+	}
+	public void setengine(int engine) {
+		this.engine = engine;
+	}
+	public int getpower() {
+		return power;
+	}
+	public void setpower(int power) {
+		this.power = power;
+	}
+	public int gettorque() {
+		return torque;
+	}
+	public void settorque(int torque) {
+		this.torque = torque;
+	}
+	public int getmileage() {
+		return mileage;
+	}
+	public void setmileage(int mileage) {
+		this.mileage = mileage;
+	}
+	public int getnoOfCylinder() {
+		return noOfCylinder;
+	}
+	public void setnoOfCylinder(int noOfCylinder) {
+		this.noOfCylinder = noOfCylinder;
+	}
+	public int getseatingCapacity() {
+		return seatingCapacity;
+	}
+	public void setseatingCapacity(int seatingCapacity) {
+		this.seatingCapacity = seatingCapacity;
+	}
+	public int getfuelTankCapacity() {
+		return fuelTankCapacity;
+	}
+	public void setfuelTankCapacity(int fuelTankCapacity) {
+		this.fuelTankCapacity = fuelTankCapacity;
+	}
+	public int getgroundClearence() {
+		return groundClearence;
+	}
+	public void setgroundClearence(int groundClearence) {
+		this.groundClearence = groundClearence;
+	}
+	public int getbootSpace() {
+		return bootSpace;
+	}
+	public void setbootSpace(int bootSpace) {
+		this.bootSpace = bootSpace;
+	}
+	public int getfuelEconomy() {
+		return fuelEconomy;
+	}
+	public void setfuelEconomy(int fuelEconomy) {
+		this.fuelEconomy = fuelEconomy;
+	}
+	public int getnumOfDoars() {
+		return numOfDoars;
+	}
+	public void setnumOfDoars(int numOfDoars) {
+		this.numOfDoars = numOfDoars;
+	}
+	public boolean getisOutOfGas() {
+		return isOutOfGas;
+	}
+	public void setisOutOfGas(boolean isOutOfGas) {
+		this.isOutOfGas = isOutOfGas;
+	}
+	public boolean getisHaveSpoiler() {
+		return isHaveSpoiler;
+	}
+	public void setisHaveSpoiler(boolean isHaveSpoiler) {
+		this.isHaveSpoiler = isHaveSpoiler;
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Encapsulation/src/com/tutorsdude/encapsulation/Runner.java
@@ -0,0 +1,111 @@
+package com.tutorsdude.encapsulation;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Car car = new Car();
+		System.out.println(car.getbrand());
+		System.out.println(car.getcarName());
+		System.out.println(car.getbodyType());
+		System.out.println(car.getdriveLine());
+		System.out.println(car.gettransmission());
+		System.out.println(car.getfuel());
+		System.out.println(car.getvariants());
+		System.out.println(car.getfeatures());
+		System.out.println(car.getsafety());
+		System.out.println(car.gettoucthscreen());
+		System.out.println(car.getwipers());
+		System.out.println(car.getsteering());
+		System.out.println(car.getinterior());
+		System.out.println(car.getsunroof());
+		System.out.println(car.getheadLamp());
+		System.out.println(car.gettailLamp());
+		System.out.println(car.getprice());
+		System.out.println(car.getcargoVolume());
+		System.out.println(car.getengine());
+		System.out.println(car.getpower());
+		System.out.println(car.gettorque());
+		System.out.println(car.getmileage());
+		System.out.println(car.getnoOfCylinder());
+		System.out.println(car.getseatingCapacity());
+		System.out.println(car.getfuelTankCapacity());
+		System.out.println(car.getgroundClearence());
+		System.out.println(car.getbootSpace());
+		System.out.println(car.getfuelEconomy());
+		System.out.println(car.getnumOfDoars());
+		System.out.println(car.getisOutOfGas());
+		System.out.println(car.getisHaveSpoiler());
+		
+		System.out.println("***************************************************************");
+		
+		car.setbrand("BMW");
+		car.setcarName("M5 Competition");
+		car.setbodyType("Sports Sedan");
+		car.setdriveLine("Four Wheel Drive");
+		car.settransmission("Manual");
+		car.setvariants("M5 Competition");
+		car.setfeatures("Drift Car");
+		car.setsafety("AirBag");
+		car.settoucthscreen("Android Touch Screen");
+		car.setwipers("Dual Wipers");
+		car.setsteering("4 Members");
+		car.setinterior("Leather");
+		car.setsunroof("No Sunroof");
+		car.setheadLamp("After Market HeadLamp");
+		car.settailLamp("Stock TailLamp");
+		car.setprice(17400000);
+		car.setcargoVolume(750);
+		car.setengine(4395);
+		car.setpower(616);
+		car.settorque(750);
+		car.setmileage(9);
+		car.setnoOfCylinder(8);
+		car.setseatingCapacity(5);
+		car.setfuelTankCapacity(68);
+		car.setgroundClearence(128);
+		car.setbootSpace(530);
+		car.setfuelEconomy(9);
+		car.setnumOfDoars(5);
+		car.setisOutOfGas(false);
+		car.setisHaveSpoiler(true);
+		
+		System.out.println("***************************************************************");
+
+		System.out.println(car.getbrand());
+		System.out.println(car.getcarName());
+		System.out.println(car.getbodyType());
+		System.out.println(car.getdriveLine());
+		System.out.println(car.gettransmission());
+		System.out.println(car.getfuel());
+		System.out.println(car.getvariants());
+		System.out.println(car.getfeatures());
+		System.out.println(car.getsafety());
+		System.out.println(car.gettoucthscreen());
+		System.out.println(car.getwipers());
+		System.out.println(car.getsteering());
+		System.out.println(car.getinterior());
+		System.out.println(car.getsunroof());
+		System.out.println(car.getheadLamp());
+		System.out.println(car.gettailLamp());
+		System.out.println(car.getprice());
+		System.out.println(car.getcargoVolume());
+		System.out.println(car.getengine());
+		System.out.println(car.getpower());
+		System.out.println(car.gettorque());
+		System.out.println(car.getmileage());
+		System.out.println(car.getnoOfCylinder());
+		System.out.println(car.getseatingCapacity());
+		System.out.println(car.getfuelTankCapacity());
+		System.out.println(car.getgroundClearence());
+		System.out.println(car.getbootSpace());
+		System.out.println(car.getfuelEconomy());
+		System.out.println(car.getnumOfDoars());
+		System.out.println(car.getisOutOfGas());
+		System.out.println(car.getisHaveSpoiler());
+		
+		
+		
+	}
+
+}

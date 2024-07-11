81ea502fb2570ce2b60a000a4ed74e73c17937e4
Manojkumar0721 <manu07y@gmail.com>
2024-07-11 19:16:16 +0530

    Updated CRUD operation

--- CrudOperation/src/com/tutorsdude/crudoperation/RepoRunner.java
+++ CrudOperation/src/com/tutorsdude/crudoperation/RepoRunner.java
@@ -3,7 +3,7 @@ package com.tutorsdude.crudoperation;
 public class RepoRunner {
 
 	public static void main(String[] args) {
-		
+
 		Repository repo = new Repository();
 		repo.saveName("Manu");
 		repo.saveName("Sarvam");
@@ -18,15 +18,7 @@ public class RepoRunner {
 		repo.saveName("Sudarshan");
 		repo.saveName("Basavaraj");
 		repo.saveName("Siddu");
-		
-//		repo.saveName(null);
-//		repo.saveName("ManojKumar");
-//		repo.saveName("PrasannaKumar");
-		repo.readName();
-		
-		
-		
-	
+
 	}
 
 }
--- CrudOperation/src/com/tutorsdude/crudoperation/Repository.java
+++ CrudOperation/src/com/tutorsdude/crudoperation/Repository.java
@@ -1,24 +1,29 @@
 package com.tutorsdude.crudoperation;
 
 public class Repository {
-	
+
 	private String[] names = new String[10];
-	
+
 	public boolean saveName(String name) {
-		if(name!=null) {
-			if(!name.isEmpty()) {
-				if(name.length()>3) {
-					if(name.length()<12) {
-			for(int i = 0; i<names.length;i++) {
-				if(names[i]==null) {
-					names[i]=name;
-					System.out.println("Name saved At index : "+ i +" : "+ names[i]);
-					return true;
-				}
-				System.out.println("Moving next place to check : " + i);
-			}
-			System.out.println("All places are filled with Name There is No Space to fill a Name");
-			return false;
+		if (name != null) {
+			if (!name.isEmpty()) {
+				if (name.length() > 3) {
+					if (name.length() < 12) {
+						if (!isExist(name)) {
+							for (int i = 0; i < names.length; i++) {
+								if (names[i] == null) {
+									names[i] = name;
+									System.out.println("Name saved At index : " + i + " : " + names[i]);
+									return true;
+								}
+								System.out.println("Moving next place to check : " + i);
+							}
+							System.out.println("All places are filled with Name There is No Space to fill a Name");
+							return false;
+
+						}
+						System.out.println("Name already exist");
+						return false;
 					}
 					System.out.println("Insertend Name Length is more than 12 plz Enter Name of Length less than 12");
 					return false;
@@ -32,14 +37,61 @@ public class Repository {
 		System.out.println("Name is Null");
 		return false;
 	}
-	
+
+	public boolean isExist(String name) {
+		if (name != null) {
+			for (int i = 0; i < names.length; i++) {
+				if (name.equals(names[i])) {
+					System.out.println("Name present");
+					return true;
+				}
+
+			}
+		}
+		return false;
+	}
+
+	public boolean update(String oldName, String newName) {
+		for (int i = 0; i < names.length; i++) {
+			if (oldName.equals(names[i])) {
+				names[i] = newName;
+				System.out.println("Name is updated");
+				return true;
+			}
+		}
+		System.out.println("Name is not updated");
+		return false;
+	}
+
+	public boolean validateName(String name) {
+		if (name != null) {
+			if (!name.isEmpty()) {
+				if (name.length() > 3 && name.length() < 12) {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean delete(String name) {
+		if (validateName(name)) {
+			for (int i = 0; i < names.length; i++) {
+				if (name.equals(names[i])) {
+					names[i] = null;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 	public void readName() {
-		for(int i = 0;i<this.names.length;i++) {
-			if(names[i]!=null) {
-			System.out.println(names[i]);
+		for (int i = 0; i < this.names.length; i++) {
+			if (names[i] != null) {
+				System.out.println(names[i]);
 			}
 		}
 	}
-	
 
 }

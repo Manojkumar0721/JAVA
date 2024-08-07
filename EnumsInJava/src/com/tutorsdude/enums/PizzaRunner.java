707ee4920954ffc3d6e7ae7ddd989aae608da055
Manojkumar0721 <manu07y@gmail.com>
2024-08-07 14:53:35 +0530

    Enums in java

--- EnumsInJava/src/com/tutorsdude/enums/PizzaRunner.java
+++ EnumsInJava/src/com/tutorsdude/enums/PizzaRunner.java
@@ -9,6 +9,7 @@ public class PizzaRunner {
 	public static void main(String[] args) {
 		
 		Pizza pizza1 = new Pizza(SizeOfPizza.PERSONAL,Diameter.TEN,Slices.FOUR);
+		Pizza pizza2 = new Pizza(SizeOfPizza.EXTRALARGE,Diameter.SIXTEEN,Slices.EIGHT);
 		
 		if(pizza1.size.detailsOfPizza.equals("Personal")) {
 			System.out.println("Size Is Personal");
@@ -18,6 +19,27 @@ public class PizzaRunner {
 			
 			System.out.println(sizeOfPizza);
 			
+		}
+		
+		if(pizza2.size.detailsOfPizza.equals("ExtraLarge")) {
+			System.out.println("Size is ExtraLarge");
+		}
+		
+		Slices slices = Slices.TEN;
+		
+		switch(slices) {
+		case FOUR:
+			System.out.println("Four slices");
+			break;
+		case SIX:
+			System.out.println("Six slices");
+			break;
+		case EIGHT:
+			System.out.println("Eight slices");
+			break;
+		case TEN:
+			System.out.println("Ten slices");
+			break;
 		}
 		
 
